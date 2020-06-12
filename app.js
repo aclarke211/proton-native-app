6d6ca762c8f3131237c09c4313af4cfd5dccae83
Alex Clarke <aclarke211@gmail.com>
2020-06-13 00:45:49 +0100

    Import a component.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import React, { Component } from "react"; // import from react
 
 import { Window, App } from "proton-native"; // import the proton-native components
 
+import TestComponent from './src/components/test/TestComponent.js';
+
 export default class Example extends Component {
   render() {
     // all Components must have a render method
@@ -9,7 +11,7 @@ export default class Example extends Component {
       <App>
         {/* you must always include App around everything */}
         <Window style={{ width: 300, height: 300, backgroundColor: "blue" }}>
-          {/* all your other components go here*/}
+          <TestComponent />
         </Window>
       </App>
     );
--- index.js
+++ index.js
@@ -2,11 +2,10 @@ import React from "react";
 import { AppRegistry } from "proton-native";
 import Example from "./app";
 
-AppRegistry.registerComponent("example", <Example />); // and finally render your main component
+AppRegistry.registerComponent("example", <Example />);
 
 // ================================================================================
-// This is for hot reloading (this will be stripped off in production by webpack)
-// THIS SHOULD NOT BE CHANGED
+/* Hot Reloading - on run in dev build */
 if (module.hot) {
   module.hot.accept(["./app"], function() {
     const app = require("./app")["default"];
